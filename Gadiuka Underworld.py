28d04f1d6a59fa9d0d78f71e004708afd07342eb
a67715643-crypto <a67715643@gmail.com>
2026-04-17 16:30:39 +0300

    part 2 of Gadiuka Cafe

---
+++ Gadiuka Underworld.py
@@ -0,0 +1,378 @@
+import colorama
+
+colorama.init()
+
+print(colorama.Fore.BLACK)
+print(colorama.Back.GREEN)
+
+print('🐍Gadiuka Cafe🐍')
+
+print(colorama.Back.LIGHTWHITE_EX)
+
+print("""instruction:
+      print takeX2 to take;
+      print jumpX2 to jump;
+      print goX2 to go;
+      print openX2 to open;
+      print fightX2 to fight.""")
+
+print(colorama.Back.YELLOW)
+print("""
+       _________
+      /        /
+     /________/
+""")
+print('Me: What is this trapdoor doing here?')
+print('trapdoor')
+print('open?')
+
+act = input()
+
+print(colorama.Back.LIGHTBLACK_EX)
+if input() == 'open':
+    print("""--------_____----
+---____--------
+--------_____-----""")
+    print(colorama.Back.RED)
+    print(colorama.Fore.LIGHTYELLOW_EX)
+    print('Welcome to the Underworld')
+    print(colorama.Fore.BLACK)
+    print(colorama.Back.LIGHTBLACK_EX)
+
+    print(colorama.Back.YELLOW)
+    print('mineshaft')
+    print('go?')
+
+    input()
+
+    if input() == 'go':
+        print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+    print(colorama.Back.LIGHTBLACK_EX)
+    print('paper')
+    print('take?')
+
+    input()
+
+    if input() == 'take':
+        print(colorama.Back.LIGHTWHITE_EX)
+        print('Something is wrong here. It looks like something... something woke up...')
+
+    print(colorama.Back.LIGHTBLACK_EX)
+    print('big cave')
+    print('jump?')
+
+    input()
+
+    if input() == 'jump':
+        print("""--------_____----
+--_____--------
+--------_______--
+------_______------
+--------_____----
+----______--------
+----------_____----
+-----_____-------
+--------_____-----
+---___-------
+--------___-----
+--------__---
+---------_-----
+
+
+_______________________/\    \ 
+You were hurt!           \    \ """)
+
+print('lower cave')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""                              \    \ 
+                               \    \ 
+                                \    \ 
+                                 \    \ 
+                                  \    \ 
+                                   \    \ 
+                                    \    \ 
+                                     \    \ ________
+                                      \     
+                                       \____________""")
+
+    print('Gadiuka\'s paper')
+    print('take?')
+
+    input()
+
+    if input() == 'take':
+        print(colorama.Back.LIGHTWHITE_EX)
+        print('Hi! Uh... you were hurt?! I remember I forgot some medicine in the end of the mineshaft! Go and take it.')
+        print(colorama.Back.LIGHTBLACK_EX)
+
+        print('mineshaft')
+        print('go?')
+
+        input()
+
+        if input() == 'go':
+            print(colorama.Back.YELLOW)
+            print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+    print(colorama.Back.LIGHTBLACK_EX)
+    print('medicine')
+    print('take?')
+
+    input()
+
+    if input() == 'take':
+        print('Now you are good!')
+
+    print('mineshaft')
+    print('go?')
+
+    input()
+
+    if input() == 'go':
+        print(colorama.Back.YELLOW)
+        print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+    print(colorama.Back.LIGHTBLACK_EX)
+    print('paper')
+    print('take?')
+
+    input()
+
+    if input() == 'take':
+        print(colorama.Back.LIGHTWHITE_EX)
+        print('Do you hear? Something big is nearby...')
+
+        print(colorama.Back.LIGHTBLACK_EX)
+        print('upper cave')
+        print('go?')
+
+        input()
+
+        if input() == 'go':
+            print("""
+    /    /
+   /    /
+  /    /
+ /    /
+/    /
+""")
+
+            print('Nice! You are up!')
+
+            print('Gadiuka\'s paper')
+            print('take?')
+
+            input()
+
+            if input() == 'take':
+                print(colorama.Back.LIGHTWHITE_EX)
+                print('I... I saw that the pʮthon disappeared... HE... HE WAS ST∞LEN! You must find him! Hu₰₰y! Hurry! Hurry!')
+
+                print(colorama.Back.LIGHTBLACK_EX)
+                print('paper')
+                print('take?')
+
+                input()
+
+                if input() == 'take':
+                    print(colorama.Back.LIGHTWHITE_EX)
+                    print('Python was stolen by unknown person! There is no information where python is.')
+
+                    print(colorama.Back.LIGHTBLACK_EX)
+                    print('mineshaft')
+                    print('go?')
+
+                    input()
+
+                    if input() == 'go':
+                        print(colorama.Back.YELLOW)
+                        print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+                    print(colorama.Back.LIGHTBLACK_EX)
+                    print('cave')
+                    print('go?')
+
+                    input()
+
+                    if input() == 'go':
+                        print("""--------____---
+--------____-----
+--_____------""")
+
+                    print('Gadiuka\'s paper')
+                    print('take?')
+
+                    input()
+
+                    if input() == 'take':
+                        print(colorama.Back.LIGHTWHITE_EX)
+                        print('I kn₯w whe¦↘ is the p⨌⁂♪on! He is ■◽ the ca™e! ∑ut... Whe⅟e is t↺e cage?... ')
+                        print(colorama.Back.LIGHTBLACK_EX)
+
+                        print('big lower cave')
+                        print('go?')
+
+                        input()
+
+                        if input() == 'go':
+                            print("""
+                                                       \    \ 
+                                                        \    \ 
+                                                         \    \ 
+                                                          \    \ 
+                                                           \    \ 
+                                                            \    \ 
+                                                             \    \      
+                                                              \    \ 
+                                                               \    \ 
+                                                                \    \ 
+                                                                 \    \ 
+                                                                  \    \ 
+                                                                   \    \ 
+                                                                    \    \ 
+                                                                     \    \___________
+                                                                      \     
+                                                                       \______________   """)
+
+                    print('paper')
+                    print('take?')
+
+                    input()
+
+                    if input() == 'take':
+                        print(colorama.Back.LIGHTWHITE_EX)
+                        print('You getting closer to it...')
+
+                        print(colorama.Back.LIGHTBLACK_EX)
+                        print('mineshaft')
+                        print('go?')
+
+                        input()
+
+                        if input() == 'go':
+                            print(colorama.Back.YELLOW)
+                            print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+                            print(colorama.Back.LIGHTBLACK_EX)
+                            print('Gadiuka\'s paper')
+                            print('take?')
+
+                            input()
+
+                            if input() == 'take':
+                                print(colorama.Back.LIGHTWHITE_EX)
+                                print('Sɐ∝ry... Bu↜... ▧ut the syʯɲols ∐re everyⅦↈ∃∫e... Thⅰs is my Ⅰast mess⋇∛e... Pæ©௹૱e... ◄ou ʥʭst f৲₸₻ ∞∝e ¾Ↄↆhon...')
+
+                    print(colorama.Back.LIGHTBLACK_EX)
+
+                    print('paper')
+                    print('take?')
+
+                    input()
+
+                    if input() == 'take':
+                        print(colorama.Back.LIGHTWHITE_EX)
+                        print('Connection to Gadiuka lost.')
+
+                        print(colorama.Back.LIGHTBLACK_EX)
+                        print('mineshaft')
+                        print('go?')
+
+                        input()
+
+                        if input() == 'go':
+                            print(colorama.Back.YELLOW)
+                            print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+                            print(colorama.Back.LIGHTBLACK_EX)
+                            print('')
+                            print(colorama.Back.YELLOW)
+                            print('Chamber')
+                            print(colorama.Back.RED)
+                            print("""_________________________
+|                       |
+|                       |
+|                       |
+|         admin         |
+|                       |
+|         /🐍/          |
+|_______________________|""")
+print('Gadiuka: HOW!? ADMIN IS ALIVE! And python! He is here! You must save him!')
+print('admin: HAHAHA! You think you won, yes? HAHAHA! You with Gadiuka are just two hopeless IDIOTS! HAHAHA! You can not save the python!')
+print('admin')
+print('fight?')
+
+input()
+
+if input() == 'fight':
+    print('admin is hurt a little')
+
+print('Me: Stop... HOW!? HOW DID YOU SURVIVED!?')
+print('admin: HAHAHA! Now no one would came for help. I won this time, not you.')
+print("""admin: fight
+       fight""")
+print('You were hurt a lot.')
+print('Gadiuka: Take the magnet! U(magnet)')
+print('magnet')
+print('fight?')
+
+input()
+
+if input() == 'fight':
+    print('admin: Why? WHY!? WHY!? WHY I AM DISAPPEARING!? HOW DID A STUPID MAGNET CAN BE MORE POWERFUL THAN MEEEEEEE!?')
+
+    print('You win!')
+    print('Gadiuka: WOW! You... You did it! Now you need to save the python.')
+    print('/🐍/')
+    print(colorama.Back.LIGHTYELLOW_EX)
+    print('key')
+    print('take?')
+
+    input()
+
+    if input() == 'take':
+        print(colorama.Back.LIGHTBLACK_EX)
+
+    print('cage')
+    print('open?')
+
+    input()
+
+    if input() == 'open':
+        print('🐍The python is free now!')
+    print('Gadiuka: Python is free! Now you need to get back to the Gadiuka Cafe!')
+
+    print(colorama.Back.YELLOW)
+    print("""
+           _________
+          /        /
+         /________/
+    """)
+    print('trapdoor')
+    print('go?')
+
+    input()
+
+    if input() == 'go':
+        print(colorama.Back.GREEN)
+
+        print('🐍Gadiuka Cafe🐍')
+
+        print(colorama.Back.YELLOW)
+        print('🐍')
+        print('Gadiuka: You were really full of determination last time! I really loved our adventure in the Underworld! But now when the admin is defea. But wait, waitted... We can live peacefully. And thank you wery much for saving the python. Now... Bye... I want to tell you that I am very happy that you survived. Because when symbols broke the connection I was scared that something can happen with you... something BAD. And I am really happy that you are alive... Now... Bye.')
+
+        coffee = 40
+        tea = 20
+        cookies = 5
+        pie = 80
